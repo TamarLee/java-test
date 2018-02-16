6cde02fefcf675a773a84f49b9b32317291f0a7e
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2018-02-17 01:43:19 +0400

    სატესტო

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main (String[] args){
+        System.out.println("გამარჯობაა");
+        pirveli();
+    }
+
+    private static void pirveli (){
+        System.out.println("თამარ");
+    }
+
+}
